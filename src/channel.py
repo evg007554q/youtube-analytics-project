670a004ce69774f8fa611e47d2b67f588f10722f
evg007 <evg007554@yandex.ru>
2023-05-25 01:35:05 +0300

    1:34

--- src/channel.py
+++ src/channel.py
@@ -14,9 +14,29 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
+        # - id канала id
+        # - название канала snippet/title
+        # - описание канала snippet/description
+        # - ссылка на канал snippet/thumbnails/default/url
+        # - количество подписчиков statistics/subscriberCount
+        # - количество видео statistics/videoCount
+        # - общее количество просмотров statistics/viewCount
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = channel['items'][0]['snippet']['title'] # MoscowPython
+        self.description = channel['items'][0]['snippet']['description']
+        self.url = channel['items'][0]['snippet']['thumbnails']['default']['url']
+        self.subscriberCount = channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = channel['items'][0]['statistics']['videoCount']
+        self.viewCount = channel['items'][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+
+        # print(channel['items'][0]['snippet']['title'])
         print(json.dumps(channel, indent=2, ensure_ascii=False))
+
+    @classmethod
+    def get_service(cls):
+        return build('youtube', 'v3', developerKey=api_key)
