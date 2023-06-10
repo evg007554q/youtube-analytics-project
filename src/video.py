9c99c7c865235d09a25d9d4ee46340f04a97a6c0
evg007 <evg007554@yandex.ru>
2023-06-10 15:45:25 +0300

    hw4

--- src/video.py
+++ src/video.py
@@ -16,19 +16,30 @@ def printj(dict_to_print: dict) -> None:
 class Video:
     def __init__(self,video_id):
 
-        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                           id=video_id
-                                           ).execute()
         self.video_id = video_id
         self.url = 'https://youtu.be/' + video_id
+
+
+        # вынесено в отдельную функцию
+        # self.title = video_response['items'][0]['snippet']['title']
+        # self.viewCount = video_response['items'][0]['statistics']['viewCount']
+        # self.likeCount = video_response['items'][0]['statistics']['likeCount']
+        # self.duration = video_response['items'][0]['statistics']['likeCount']
+
+        self.set_attributes_video()
+
+    def __str__(self):
+        return f'{self.title}'
+
+    def set_attributes_video(self):
+        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                               id=self.video_id
+                                               ).execute()
+
         self.title = video_response['items'][0]['snippet']['title']
         self.viewCount = video_response['items'][0]['statistics']['viewCount']
         self.likeCount = video_response['items'][0]['statistics']['likeCount']
         self.duration = video_response['items'][0]['statistics']['likeCount']
-        printj(video_response)
-
-    def __str__(self):
-        return f'{self.title}'
 
 class PLVideo(Video):
     def __init__(self, video_id, channel_id):
