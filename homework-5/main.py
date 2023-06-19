e99835769d557fda30c11013851a27ff2de1838d
evg007 <evg007554@yandex.ru>
2023-06-19 22:57:25 +0300

    hw5 + Замечания 13.06.2023

--- homework-5/main.py
+++ homework-5/main.py
@@ -5,14 +5,23 @@ from src.playlist import PlayList
 if __name__ == '__main__':
     pl = PlayList('PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw')
 
-    # assert pl.title == "Moscow Python Meetup №81"
-    # assert pl.url == "https://www.youtube.com/playlist?list=PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw"
-    #
-    # duration = pl.total_duration
-    # assert str(duration) == "1:49:52"
-    # assert isinstance(duration, datetime.timedelta)
-    # assert duration.total_seconds() == 6592.0
-    #
-    # assert pl.show_best_video() == "https://youtu.be/cUGyMzWQcGM"
 
-    print(pl.__dict__)
+    assert pl.title == "Moscow Python Meetup №81"
+    assert pl.url == "https://www.youtube.com/playlist?list=PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw"
+
+    duration = pl.total_duration
+
+    assert str(duration) == "1:49:52"
+    assert isinstance(duration, datetime.timedelta)
+    assert duration.total_seconds() == 6592.0
+
+    assert pl.show_best_video() == "https://youtu.be/cUGyMzWQcGM"
+
+    # print(pl.__dict__)
+
+    # aaa = '1'
+    # while aaa == '1':
+    #     pl.set_attributes_playlist()
+    #     print(datetime.datetime.now())
+    #     print(pl.best_like_count)
+    #     aaa = input("-")
--- src/playlist.py
+++ src/playlist.py
@@ -4,74 +4,75 @@ from googleapiclient.discovery import build
 
 import isodate
 # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
-api_key: str = os.getenv('YT_API_KEY')
-# print(api_key)
 
-# создать специальный объект для работы с API
-youtube = build('youtube', 'v3', developerKey=api_key)
+class APIMixin:
+    api_key: str = os.getenv('YT_API_KEY')
+    # print(api_key)
 
-def printj(dict_to_print: dict) -> None:
-    """Выводит словарь в json-подобном удобном формате с отступами"""
-    print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+    @classmethod
+    def get_service(cls):
+        """Объект для работы с API"""
+        service = build('youtube', 'v3', developerKey = cls.api_key)
+        return service
 
-class PlayList:
-    # title: object
-
+class PlayList(APIMixin):
+    """"""
     def __init__(self, playlist_id):
         self.playlist_id = playlist_id
-        self.url = f'https://www.youtube.com/playlist?list={playlist_id}'
-        self.__total_duration = None
-        self.__show_best_video = None
-        self.video_ids = None
-        self.title = None
-        # playlist_y = youtube.playlists().list(id=playlist_id,
-        #                                       part='contentDetails,snippet',
-        #                                       maxResults=50,
-        #                                       ).execute()
-        self.set_attributes_playlist()
+        self._init_from_api()
 
-    def set_attributes_playlist(self):
+    def _init_from_api(self):
+        """Получаем данные по API и инициализируем ими экземпляр класса."""
         playlist_id = self.playlist_id
-
-        playlist_y = youtube.playlists().list(id=playlist_id,
+        self.url = f'https://www.youtube.com/playlist?list={playlist_id}'
+        playlist_y = self.get_service().playlists().list(id=playlist_id,
                                               part='contentDetails,snippet',
                                               maxResults=50,
                                               ).execute()
+        self.title = playlist_y['items'][0]['snippet']['title']
+        # self.total_duration()
+        # self.show_best_video()
 
-        # self.title = playlist_y['items'][0]['snippet']['title']
-
-        playlist_y = youtube.playlistItems().list(playlistId=playlist_id,
-                                                  part='contentDetails,snippet',
-                                                  maxResults=50,
-                                                  ).execute()
-
-        self.video_ids: list[str] = [video['contentDetails']['videoId'] for video in playlist_y['items']]
-
-        video_response = youtube.videos().list(part='contentDetails,statistics',
-                                               id=','.join(self.video_ids)
-                                               ).execute()
-
+    @property
+    def total_duration(self):
+        """Возвращает суммарную длительность плей-листа в формате 'datetime.timedelta' (hh:mm:ss))."""
+        video_response = self._get_playlist_videos()
         duration = isodate.parse_duration('PT0M0S')
+        for video in video_response['items']:
+             # YouTube video duration is in ISO 8601 format
+             iso_8601_duration = video['contentDetails']['duration']
+             duration += isodate.parse_duration(iso_8601_duration)
+
+        # self.__total_duration = duration
+
+        return duration
+    def show_best_video(self) -> None:
+        """Выводит ссылку на самое залайканое видео в плейлисте."""
+        video_response = self._get_playlist_videos()
+
         best_like_count = 0
         self.__show_best_video = ''
 
         for video in video_response['items']:
-            # YouTube video duration is in ISO 8601 format
-            iso_8601_duration = video['contentDetails']['duration']
-            duration += isodate.parse_duration(iso_8601_duration)
-            # print(type(duration))
             like_count = int(video['statistics']['likeCount'])
             if like_count > best_like_count:
                 self.__show_best_video = video['id']
                 best_like_count = like_count
-                # print(type(self.__show_best_video) )
+                self.best_like_count = best_like_count
+        return 'https://youtu.be/' + self.__show_best_video
 
-        self.__total_duration = duration
+    def _get_playlist_videos(self):
+        playlist_y = self.get_service().playlistItems().list(playlistId=self.playlist_id,
+                                                   part='contentDetails,snippet',
+                                                   maxResults=50,
+                                                   ).execute()
 
-    @property
-    def total_duration(self):
-        return self.__total_duration
+        video_ids: list[str] = [video['contentDetails']['videoId'] for video in playlist_y['items']]
 
-    # @property
-    def show_best_video(self):
-        return 'https://youtu.be/' +  self.__show_best_video
+        video_response = self.get_service().videos().list(part='contentDetails,statistics',
+                                            id=','.join(video_ids)
+                                                ).execute()
+        return video_response
+
+
+
