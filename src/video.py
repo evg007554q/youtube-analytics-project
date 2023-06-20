31ec0e3285a73ff48c4b7ff1f4341b213128196d
evg007 <evg007554@yandex.ru>
2023-06-20 11:15:43 +0300

    hw6

--- src/video.py
+++ src/video.py
@@ -8,39 +8,38 @@ api_key: str = os.getenv('YT_API_KEY')
 
 # создать специальный объект для работы с API
 youtube = build('youtube', 'v3', developerKey=api_key)
-print(api_key)
 
-def printj(dict_to_print: dict) -> None:
-    """Выводит словарь в json-подобном удобном формате с отступами"""
-    print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+# print(api_key)
+#
+# def printj(dict_to_print: dict) -> None:
+#     """Выводит словарь в json-подобном удобном формате с отступами"""
+#     print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
 
 class Video:
     def __init__(self,video_id):
-
         self.video_id = video_id
-        self.url = 'https://youtu.be/' + video_id
-
-
-        # вынесено в отдельную функцию
-        # self.title = video_response['items'][0]['snippet']['title']
-        # self.viewCount = video_response['items'][0]['statistics']['viewCount']
-        # self.likeCount = video_response['items'][0]['statistics']['likeCount']
-        # self.duration = video_response['items'][0]['statistics']['likeCount']
-
         self.set_attributes_video()
 
     def __str__(self):
         return f'{self.title}'
 
     def set_attributes_video(self):
-        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=self.video_id
-                                               ).execute()
+        # попытка
+        try:
+            video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                   id=self.video_id
+                                                   ).execute()
+            self.url = 'https://youtu.be/' + self.video_id
+            self.title = video_response['items'][0]['snippet']['title']
+            self.viewCount = video_response['items'][0]['statistics']['viewCount']
+            self.like_count = video_response['items'][0]['statistics']['likeCount']
+        #Исключение
+        except:
+            self.url =       None
+            self.title =     None
+            self.viewCount = None
+            self.like_count = None
 
-        self.title = video_response['items'][0]['snippet']['title']
-        self.viewCount = video_response['items'][0]['statistics']['viewCount']
-        self.likeCount = video_response['items'][0]['statistics']['likeCount']
-        self.duration = video_response['items'][0]['statistics']['likeCount']
 
 class PLVideo(Video):
     def __init__(self, video_id, channel_id):
